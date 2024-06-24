bf15b6a3bdf1a8c20fbf1922520f4b351fedf153
Parziwal-19 <siddhantb1905@gmail.com>
2024-06-24 15:49:51 +0530

    only hello world in index

--- src/pages/api/todo.js
+++ src/pages/api/todo.js
@@ -1,6 +1,21 @@
-export default function handler(req, res) {
+import { createClient } from '@supabase/supabase-js'
+
+async function fetchData() {
+  const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+  const { data: fetchedData, error } = await supabase
+    .from('todo-table')
+    .select();
+  if (error) {
+    console.error('Error fetching data:', error);
+  } else {
+    return fetchedData;
+  }
+}
+
+export default async function handler(req, res) {
   if (req.method === 'GET') {
-    res.status(200).json({ message: 'Hello World' });
+    var data = await fetchData();
+    res.status(200).json(data);
   } else {
     res.setHeader('Allow', ['GET']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
