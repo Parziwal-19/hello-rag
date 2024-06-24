53e7ab5e2e5fc4185121c9b3da2b8e20ad32f246
Parziwal-19 <siddhantb1905@gmail.com>
2024-06-24 13:45:59 +0530

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { createClient } from '@supabase/supabase-js'
 
 
-const supabase = createClient('hello-rag-seven.vercel.app', 'public-anon-key')
+const supabase = createClient('https://wkjrdowvrrbceqyugbhf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndranJkb3d2cnJiY2VxeXVnYmhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkyMTQxMTYsImV4cCI6MjAzNDc5MDExNn0.ETBAowuq3xSZys0M52Z-ILYcxW7blnPuQH9RLEnkINc')
 const { data, error } = await supabase
   .from('todo-list')
   .select()
