7dd260794cff22a51d48fc26155dc2a51aa1747d
Parziwal-19 <siddhantb1905@gmail.com>
2024-06-24 12:15:15 +0530

    init

--- removeCrawled.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const idsToProcess = JSON.parse(fs.readFileSync("idsToProcess.json", "utf8"));
-const toRemove = JSON.parse(fs.readFileSync("toRemove.json", "utf8"));
-
-const nonCrawledPages = idsToProcess.filter((id) => !toRemove.includes(id));
-console.log(nonCrawledPages.length());
-
-fs.writeFileSync("idsToProcessNew.json", JSON.stringify(nonCrawledPages));
